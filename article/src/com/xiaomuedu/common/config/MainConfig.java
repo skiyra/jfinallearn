fa30ee97e4947db88874ec0cbfa8532d268b4a57
skiyra <myb4562@163.com>
2018-04-24 00:16:54 +0800

    learn jfinal

--- article/src/com/xiaomuedu/common/config/MainConfig.java
+++ article/src/com/xiaomuedu/common/config/MainConfig.java
@@ -95,7 +95,8 @@ public class MainConfig extends JFinalConfig {
 	}
 	
 	public static void main(String[] args) {
-		JFinal.start("WebRoot", 80, "/", 5);
+		//JFinal.start("WebRoot", 80, "/", 5);
+		JFinal.start("WebRoot", 80, "/");
 	}
 	
 
